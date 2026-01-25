547203378a0afd25d3b269c8002d339a697d1219
jneberhard <jreberhard3@gmail.com>
2026-01-24 22:56:18 -0700

    fix for db

--- nextjs-dashboard/app/dashboard/customers/page.tsx
+++ nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -3,7 +3,8 @@ export const dynamic = 'force-dynamic';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Customers | Acme Dashboard',
+  title: 'Customers | Acme Dashboard', // Page Title
+description: 'Manage your customers in the Acme Dashboard.', // Page Description
 };
 
 export default function Page() {
